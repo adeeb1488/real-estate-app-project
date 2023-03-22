da6fc8f6ea24ddf1f964c42b5a5de11389cc73f5
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-22 01:53:25 -0400

    Creating footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Box} from '@chakra-ui/react'
+const Footer = () => {
+  return (
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.100">
+        2023 real-estate-app.
+    </Box>
+  )
+}
+
+export default Footer
