e40de726fe269d16095b2bd40b514a8809597300
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-24 23:19:52 -0400

    Initial code for search filters on search page, adding search filter data

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,10 +3,20 @@ import {Flex, Select, Box, Text, Input,Spinner, Icon, Button} from '@chakra-ui/r
 import {useRouter} from 'next/router'
 import {MdCancel} from 'react-icons/md'
 import Image from 'next/image';
-
+import { filterData, getFilterValues } from '@/utils/filterData';
 const SearchFilters = () => {
+  const [filters, setFilters] = useState(filterData)
+  const searchProperties=()=>{
+    
+  }
   return (
-    <div>SearchFilters</div>
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter)=>(
+        <Box key={filter.queryName}>
+          <Select onChange={(e)=>searchProperties()}></Select>
+        </Box>
+      ))}
+    </Flex>
   )
 }
 
