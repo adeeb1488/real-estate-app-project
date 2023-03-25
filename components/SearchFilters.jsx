92533f07fa95bfbc40c96349d1edf3b73ecd283a
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-24 23:26:29 -0400

    Adding search filter options on for-sale and for-rent properties page

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -6,14 +6,18 @@ import Image from 'next/image';
 import { filterData, getFilterValues } from '@/utils/filterData';
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData)
-  const searchProperties=()=>{
-    
+  const searchProperties=(filterValues)=>{
+
   }
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
       {filters.map((filter)=>(
         <Box key={filter.queryName}>
-          <Select onChange={(e)=>searchProperties()}></Select>
+          <Select 
+          placeholder={filter.placeholder}
+          w="fit-content"
+          p="2"   
+          onChange={(e)=>searchProperties({[filter.queryName]: e.target.value})}></Select>
         </Box>
       ))}
     </Flex>
